30b5f089d57a9fd25f50d067716bfcb6a399c280
root <root@DESKTOP-JMDF9KN.localdomain>
2022-12-07 15:04:00 +0700

    quiz

--- pert8-14/user_service/app/api/db.py
+++ pert8-14/user_service/app/api/db.py
@@ -1,2 +1,2 @@
 from pymongo import MongoClient
-conn = MongoClient('mongodb://root:p455w0rd@172.30.247.244:27017/users?authSource=admin')
+conn = MongoClient('mongodb://root:p455w0rd@172.20.46.222:27017/users?authSource=admin')
